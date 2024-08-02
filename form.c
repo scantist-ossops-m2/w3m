ffe1bc9838823e6d75dc23346b5a1a139a072cfd
TL-Yao <tongle@scntist.com>
2024-08-02 10:41:54 +0000

    commit patch 4996348

--- form.c
+++ form.c
@@ -458,6 +458,8 @@ formUpdateBuffer(Anchor *a, Buffer *buf, FormItemList *form)
 	}
 	else
 #endif				/* MENU_SELECT */
+	    if (!form->value)
+		break;
 	    p = form->value->ptr;
 	l = buf->currentLine;
 	if (!l)
